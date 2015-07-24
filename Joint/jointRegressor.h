63aa589f5b45535d2c1884cd15574ab1f6a6d0a4
Toxic <toxic.xc@sjtu.edu.cn>
2015-07-24 11:17:39 +0800

    add jointRegressor.h

--- Joint/jointRegressor.h
+++ Joint/jointRegressor.h
@@ -13,6 +13,38 @@
 class JointRegressor {
 public:
 	std::vector<RandomForest> forests;
+	std::vector<std::vector<struct model*>> models;
+	Shape meanShape;
+	std::vector<Sample> samples;
+	int totalStages;
+public:
+	JointRegressor() {
+		totalStages = GlobalParams::stages;
+		forests.resize(totalStages);
+		models.resize(totalStages);
+	}
+	~JointRegressor() {
+
+	}
+	
+	feature_node ** DeriveBinaryFeat(
+		const RandomForest& forest,
+		const std::vector<Sample>& samples
+		);
+	void ReleaseFeatureSpace(
+		feature_node ** binfeatures,
+		int numSamples);
+	int GetCodeFromTree(
+		const Tree& tree,
+		const vector<Sample>& samples
+		);
+	void GlobalRegression(
+		feature_node ** binfeatures,
+		const std::vector<Sample> &samples,
+		int stages
+		);
+
+	void Train(const std::vector<Sample>& samples);
 };
 
 #endif
